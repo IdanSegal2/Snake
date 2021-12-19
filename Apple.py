bc098de58380826057b3a6f289e64ed9e625881b
noyagid <noya.gideoni@gmail.com>
2021-12-19 10:50:27 +0200

    Class Apple is done

--- Apple.py
+++ Apple.py
@@ -5,11 +5,17 @@ class Apple:
     def __init__(self):
         # (x,y,score) - Random location on the board and initial score
         apple_parameters = get_random_apple_data()
+        # Unpacking apple_parameters tuple into coordinates and score
         self.x, self.y, self.score = apple_parameters
 
-
     def get_apple_coord(self):
+        """
+        :return: tuple of apple coordinates (x, y)
+        """
         return self.x, self.y
 
     def get_apple_score(self):
+        """
+        :return: int of random score given to certain apple
+        """
         return self.score
